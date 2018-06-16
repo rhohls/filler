9f819287633cb88994f679b6e160986876ded018
Richard HOHLS <rhohls@e4r15p7.wethinkcode.co.za>
2018-06-16 15:37:14 +0200

    saturday

--- testing/fillertest.h
+++ testing/fillertest.h
@@ -6,11 +6,11 @@
 /*   By: rhohls <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/16 11:27:48 by rhohls            #+#    #+#             */
-/*   Updated: 2018/06/16 11:31:15 by rhohls           ###   ########.fr       */
+/*   Updated: 2018/06/16 15:36:41 by rhohls           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLER_TEST_H
 # define FILLER_TEST_H
 
-# define FILLER_PATH 
+
